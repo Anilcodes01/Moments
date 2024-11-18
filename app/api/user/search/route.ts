ed9a62686f45b317b4d18abad44b60d46910e610
Anil Pal <anilcodes01@gmail.com>
2024-11-18 11:44:22 +0530

    fixed bug

--- app/api/user/search/route.ts
+++ app/api/user/search/route.ts
@@ -33,6 +33,7 @@ export async function GET(req: Request) {
 
     return NextResponse.json(users, { status: 200 });
   } catch (error) {
+    console.log(error);
     return NextResponse.json(
       {
         error: "Something went wrong",
