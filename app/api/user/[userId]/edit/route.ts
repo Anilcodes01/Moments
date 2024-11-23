5a23d1bf2fe994b6dd9cf68f21c4da36ce77ce88
Anil Pal <anilcodes01@gmail.com>
2024-11-23 15:17:13 +0530

    fixed deployment errors

--- app/api/user/[userId]/edit/route.ts
+++ app/api/user/[userId]/edit/route.ts
@@ -120,6 +120,7 @@ export async function POST(
     return NextResponse.json(
       {
         message: "Error while editing the profile, Please try again later...",
+        error,
       },
       { status: 500 }
     );
